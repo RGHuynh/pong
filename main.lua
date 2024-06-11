f87c76d6cc559d53d45275f0c99a4d78b0f3fef3
Randall Huynh <18519996+RGHuynh@users.noreply.github.com>
2024-06-11 19:31:45 -0400

    add basic

--- main.lua
+++ main.lua
@@ -1,5 +1,9 @@
 function love.load()
-
+  love.window.setMode(1000, 1000, {
+    resizable = false,
+    fullscreen = false,
+    vsync = true
+  })
 end
 
 function love.update()
@@ -7,6 +11,6 @@ function love.update()
 end
 
 function love.draw()
-
+  love.graphics.printf('hello world', 0, 0, 1000, 'center')
 end
 
