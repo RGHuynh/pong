847e0121a8e3b9be3752c032c6ab0a5fd63ceedc
Mrbrokenlegs <randall@lunchbox.io>
2024-06-11 21:27:43 -0400

    Add text

--- main.lua
+++ main.lua
@@ -1,15 +1,34 @@
+local push = require 'libraries.push'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+
 function love.load()
-  love.window.setMode(1000, 1000, {
+
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
     resizable = false,
     fullscreen = false,
     vsync = true
   })
 end
 
-function love.update()
-
+function love.keypressed(key)
+  if key == 'escape' then
+    love.event.quit()
+  end
 end
 
+-- function love.update(dt)
+
+-- end
+
 function love.draw()
-  love.graphics.printf('hello world', 0, 0, 1000, 'center')
+  push:apply('start')
+  love.graphics.printf('hello world', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+  push:apply('end')
 end
