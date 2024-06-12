442e0d23e62f78184333caccfcda06df41194bdb
Mrbrokenlegs <randall@lunchbox.io>
2024-06-11 21:37:56 -0400

    setup pong board

--- main.lua
+++ main.lua
@@ -8,13 +8,17 @@ VIRTUAL_HEIGHT = 243
 
 
 function love.load()
-
   love.graphics.setDefaultFilter('nearest', 'nearest')
   push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
     resizable = false,
     fullscreen = false,
     vsync = true
   })
+
+
+  smallFont = love.graphics.newFont('font.ttf', 8)
+
+  love.graphics.setFont(smallFont)
 end
 
 function love.keypressed(key)
@@ -29,6 +33,14 @@ end
 
 function love.draw()
   push:apply('start')
-  love.graphics.printf('hello world', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+
+  -- love.graphics.printf('hello world', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+
+  love.graphics.rectangle('fill', 10, 30, 5, 20)
+
+  love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+
+  love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 5, 5)
   push:apply('end')
 end
